c3a15a4edf532a2765081a08c3c0f31b43f9538d
Patryk Wychowaniec <pwychowaniec@pm.me>
2022-02-25 22:08:00 +0100

    Add Bevy skeleton

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShell = {
+      "x86_64-linux" =
+        let
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+          };
+
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            alsaLib
+            alsaLib
+            libudev
+            libxkbcommon
+            pkg-config
+            udev
+            vulkan-loader
+            wayland
+          ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${with pkgs; lib.makeLibraryPath [
+              alsaLib
+              libxkbcommon
+              udev
+              vulkan-loader
+              wayland
+            ]}"
+          '';
+        };
+    };
+  };
+}
