81735b9f4545db692377d740a26fb86d719bcfb0
Dzejkop <jakubtrad@gmail.com>
2022-02-26 13:49:44 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use bevy::prelude::*;
+use rand::{thread_rng, Rng};
 
 #[derive(Component, Debug)]
-struct FactoryNode;
+struct FactoryNode {
+    time_to_spawn: f32,
+    timer: f32,
+}
+
+#[derive(Component, Debug)]
+struct Node;
+
+#[derive(Component, Debug)]
+struct Cell;
 
 fn setup(mut commands: Commands, assets: Res<AssetServer>) {
     let sprite_handle = assets.load("placeholder_circle.png");
@@ -9,14 +19,34 @@ fn setup(mut commands: Commands, assets: Res<AssetServer>) {
     commands
         .spawn()
         .insert_bundle(OrthographicCameraBundle::new_2d())
-        .insert(Transform::from_xyz(0.0, 0.0, 1000.0))
-        .insert(FactoryNode);
+        .insert(Transform::from_xyz(0.0, 0.0, 1000.0));
 
-    commands.spawn_bundle(SpriteBundle {
-        texture: sprite_handle.clone(),
-        transform: Transform::default(),
-        ..Default::default()
-    });
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: sprite_handle.clone(),
+            transform: Transform::default(),
+            sprite: Sprite {
+                color: Color::RED,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(FactoryNode {
+            time_to_spawn: 1.0,
+            timer: 1.0,
+        });
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: sprite_handle.clone(),
+            transform: Transform::from_xyz(100.0, 0.0, 0.0),
+            sprite: Sprite {
+                color: Color::BLUE,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Node);
 }
 
 fn main() {
@@ -24,11 +54,44 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_system(factory_node_system)
+        .add_system(brownian_motion_system)
         .run();
 }
 
-fn factory_node_system(mut query: Query<&mut FactoryNode>) {
-    for _ in &mut query.iter_mut() {
-        // Do something with factory nodes
+fn brownian_motion_system(mut query: Query<(&Cell, &mut Transform)>) {
+    let mut rng = thread_rng();
+    for (_, mut transform) in query.iter_mut() {
+        transform.translation +=
+            Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), 0.0);
+    }
+}
+
+fn factory_node_system(
+    mut commands: Commands,
+    time: Res<Time>,
+    assets: Res<AssetServer>,
+    mut query: Query<&mut FactoryNode>,
+) {
+    for mut factory_node in &mut query.iter_mut() {
+        factory_node.timer -= time.delta_seconds();
+
+        if factory_node.timer <= 0.0 {
+            factory_node.timer = factory_node.time_to_spawn;
+
+            // TODO: Do not load sprite every time, wtf?
+            let sprite_handle = assets.load("placeholder_square.png");
+
+            commands
+                .spawn_bundle(SpriteBundle {
+                    texture: sprite_handle.clone(),
+                    transform: Transform::from_xyz(100.0, 0.0, 0.0),
+                    sprite: Sprite {
+                        color: Color::GREEN,
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                })
+                .insert(Cell);
+        }
     }
 }
