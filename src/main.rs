c28911ab5185cae44a15e16807bdcfe9299b57ed
Dzejkop <jakubtrad@gmail.com>
2022-02-26 13:36:19 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 use bevy::prelude::*;
 
-fn hello_world() {
-    println!("hello world!");
+#[derive(Component, Debug)]
+struct FactoryNode;
+
+fn setup(mut commands: Commands, assets: Res<AssetServer>) {
+    let sprite_handle = assets.load("placeholder_circle.png");
+
+    commands
+        .spawn()
+        .insert_bundle(OrthographicCameraBundle::new_2d())
+        .insert(Transform::from_xyz(0.0, 0.0, 1000.0))
+        .insert(FactoryNode);
+
+    commands.spawn_bundle(SpriteBundle {
+        texture: sprite_handle.clone(),
+        transform: Transform::default(),
+        ..Default::default()
+    });
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(hello_world)
+        .add_startup_system(setup)
+        .add_system(factory_node_system)
         .run();
+}
+
+fn factory_node_system(mut query: Query<&mut FactoryNode>) {
+    for _ in &mut query.iter_mut() {
+        // Do something with factory nodes
+    }
 }
