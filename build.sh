806dcab887f134016928f7df8f6133ad4e151f5a
Pawel Rynowiecki <pawel.rynowiecki@anixe.pl>
2022-03-04 20:17:59 +0100

    Improve build script

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/unfair_advantage.wasm
+zip immune assets/* levels/* out/* index.html
